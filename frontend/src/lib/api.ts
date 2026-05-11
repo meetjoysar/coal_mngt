246db96ea7f83d8ba454e8a2d69b794e491e1c04
Meet Joysar <meetjoysar7s@gmail.com>
2026-05-11 15:42:10 +0530

    trying to figure the vercel fetch error 3

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -2,6 +2,7 @@ import type { ApiItem, ApiList } from "../types";
 
 // const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "http:///api";
 const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4010";
+//trying
 
 type RequestOptions = {
   method?: "GET" | "POST" | "PUT" | "DELETE";
